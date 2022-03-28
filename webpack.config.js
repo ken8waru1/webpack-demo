35d91766b01e9df0abf05c599ee463292a0603c4
ken8waru1 <ken8waru1@gmail.com>
2022-03-28 15:41:43 -0700

    Add loaders to webpack and some style updates to app

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,49 @@
 const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-	entry: "./src/index.js",
-	output: {
-		filename: "bundle.js",
-		path: path.resolve(__dirname, "./dist"),
-		publicPath: "dist/",
-	},
-	mode: "none",
-	module: {
-		rules: [
-			{
-				test: /\.(png|jpg)$/,
-				type: "asset",
-				parser: {
-					dataUrlCondition: {
-						maxSize: 3 * 1024,
-					},
-				},
-			},
-			{
-				test: /\.txt/,
-				type: "asset/source",
-			},
-		],
-	},
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+    publicPath: "dist/",
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset/inline",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024,
+          },
+        },
+      },
+      {
+        test: /\.txt/,
+        type: "asset/source",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+            plugins: ["@babel/plugin-proposal-class-properties"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [new TerserPlugin()],
 };
