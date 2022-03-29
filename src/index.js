17ddc2b30b8e37186af933377f8abd518a1c8358
ken8waru1 <ken8waru1@gmail.com>
2022-03-29 03:01:08 -0700

    Add content hashing and dist folder cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import HelloWorldButton from "./components/hello-world-button/hello-world-button.js";
+import Heading from "./components/heading/heading.js";
 import addImage from "./add-image.js";
 
 const helloWorldButton = new HelloWorldButton();
+const heading = new Heading();
+
+heading.render();
 helloWorldButton.render();
 addImage();
